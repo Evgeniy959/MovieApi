a415c2065e8daf9e159dc778b639f4c51756bb5b
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-09-20 20:11:34 +0700

    Add project files.

---
+++ MovieApi/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieApi.Models;
+using MovieApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MovieApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMovieApiService movieApiService;
+
+        public HomeController(ILogger<HomeController> logger, IMovieApiService movieApiService)
+        {
+            _logger = logger;
+            this.movieApiService = movieApiService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public async Task<IActionResult> Search(string title)
+        {
+            /*HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"https://omdbapi.com/?apikey=5b9b7798&s={title}");
+            var result = await response.Content.ReadAsStringAsync();
+            //Console.WriteLine(result);*/
+            //Console.WriteLine(title +"-"+age);
+            //ViewBag.Result = result;
+            var result = await movieApiService.SearchByTitle(title);
+            ViewBag.MovieTitle = title;
+            return View(result);
+        }
+        public async Task<IActionResult> Details(string id)
+        {
+            Details details = await movieApiService.SearchById(id);
+            Console.WriteLine("999999999 - " + id);
+            return View(details);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieApi/Services/MovieApiService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Options;
+using MovieApi.Models;
+using MovieApi.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MovieApi.Services
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public string BaseUrl { get; }
+        public string ApiKey { get; }
+        private HttpClient httpClient;
+
+        public MovieApiService(IHttpClientFactory httpClientFactory, IOptions<MovieApiOptions> options)
+        {
+            /*BaseUrl = "https://omdbapi.com/";
+            ApiKey = "5b9b7798";*/
+
+            BaseUrl = options.Value.BaseUrl;
+            ApiKey = options.Value.ApiKey;
+            httpClient = httpClientFactory.CreateClient();
+            //httpClient = new HttpClient();
+        }
+
+        /*public async Task<string> SearchByTitle(string title)
+        {
+            var response = await httpClient.GetAsync($"{BaseUrl}?apikey={ApiKey}&s={title}");
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+
+            return result;
+        }*/
+        public async Task<MovieApiResponse> SearchByTitle(string title)
+        {
+            var response = await httpClient.GetAsync($"{BaseUrl}?apikey={ApiKey}&s={title}");
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<MovieApiResponse>(json);
+            if (result.Response == "False") 
+            {
+                throw new Exception(result.Error);
+            }
+            return result;
+        }
+
+        public async Task<Details> SearchById(string id)
+        {
+        //https://omdbapi.com/?i=tt0286716&apikey=5b9b7798&page=2
+            var response = await httpClient.GetAsync($"{BaseUrl}?apikey={ApiKey}&i={id}");
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<Details>(json);
+            if (result.Response == "False")
+            {
+                throw new Exception(result.Error);
+            }
+            return result;
+        }
+    }
+}
+//https://omdbapi.com/?apikey=5b9b7798&s='{title}'
