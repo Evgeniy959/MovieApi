4712fcc624691e0834ccb276556302f26bbb18c6
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-09-23 07:29:06 +0700

    update

--- MovieApi/Models/MovieApiResponse.cs
+++ MovieApi/Models/MovieApiResponse.cs
@@ -2,7 +2,7 @@
 {
     public class MovieApiResponse
     {
-        public Search[] Search { get; set; }
+        public Details[] Search { get; set; }
         public string totalResults { get; set; }
         public string Response { get; set; }
         public string Error { get; set; }
